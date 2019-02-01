aaf0365ea015fa42668d357c2838113e12b9c584
littlemooon <fredwright0@gmail.com>
2019-02-02 03:01:21 +0700

    🎉 show auth user info

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,8 @@
+import d from 'dot-prop'
 import Link from 'next/link'
 import { Component } from 'react'
 import Layout from '../layout'
+import { AuthContext } from '../state/AuthState'
 
 class AboutPage extends Component<{ isServer: boolean }> {
   public static getInitialProps() {
@@ -11,16 +13,34 @@ class AboutPage extends Component<{ isServer: boolean }> {
   public render() {
     return (
       <Layout>
-        <section>
-          <p>
-            This is another page of the SSR example, you accessed it{' '}
-            <strong>{this.props.isServer ? 'server' : 'client'} side</strong>.
-          </p>
-          <p>You can reload to see how the page change.</p>
-          <Link href="/">
-            <a>Go to Home</a>
-          </Link>
-        </section>
+        <AuthContext.Consumer>
+          {authState => (
+            <>
+              <section>
+                <p>
+                  This is another page of the SSR example, you accessed it{' '}
+                  <strong>
+                    {this.props.isServer ? 'server' : 'client'} side
+                  </strong>
+                  .
+                </p>
+                <p>You can reload to see how the page change.</p>
+                <Link href="/">
+                  <a>Go to Home</a>
+                </Link>
+              </section>
+
+              {authState.user && (
+                <section>
+                  {Object.keys(authState.user).map(k => {
+                    const v = d.get(authState, `user.${k}`)
+                    return <p key={k}>{`${k}: ${JSON.stringify(v)}`}</p>
+                  })}
+                </section>
+              )}
+            </>
+          )}
+        </AuthContext.Consumer>
       </Layout>
     )
   }
